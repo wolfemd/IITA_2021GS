184aa51455d5649b266cd045f4bc64c3c90759d2
wolfemd <wolfemd@gmail.com>
2021-07-07 11:35:15 -0400

    Update predCrossMeans: predType="BV" or "TGV" instead of modelType="A" or "AD". See comments for details. Most utility functions from the OG predCrossVar pkg now included unaltered. Lastly: furrr/futures now use plan(multicore), no memory constraint.

--- code/predCrossVar.R
+++ code/predCrossVar.R
@@ -32,7 +32,9 @@ predCrossVars<-function(CrossesToPredict,modelType,
 
 
   # Set-up a loop over the crosses
-  require(furrr); options(mc.cores=ncores); plan(multicore)
+  require(furrr); plan(multicore, workers = ncores)
+  options(future.globals.maxSize=+Inf); options(future.rng.onMisuse="ignore")
+
   crossespredicted<-CrossesToPredict %>%
     mutate(predVars=future_pmap(.,
                                 predOneCross,
@@ -195,8 +197,15 @@ calcCrossLD<-function(sireID,damID,recombFreqMat,haploMat){
 # does x %*% D %*% t(x) faster
 quadform<-function(D,x,y){ return(as.numeric(colSums(x*(D%*%y)))) }
 
-
-predCrossMeans<-function(CrossesToPredict,modelType,
+# Warning: prediction of meanTGV with F-M Eqn. 14.6 appropriate
+## only using a+d partition not allele sub. + dom. dev.;
+## genotypic NOT classical in terms used by Vitezica et al. 2013.
+## For that reason, predCrossMeans has a "predType" not a "modelType" argument
+## predType="TGV" uses F-M Eqn. 14.6 and takes add and dom effect
+## predType="BV" input should be allele subst. effs, computes mid-parent GEBV
+## there is no equivalent to predicting the dominance variance for the mean
+## thus the difference from the predCrossVars() function.
+predCrossMeans<-function(CrossesToPredict,predType,
                          AddEffectList,DomEffectList=NULL,
                          doseMat,
                          ncores=1,...){
@@ -205,32 +214,216 @@ predCrossMeans<-function(CrossesToPredict,modelType,
   parents<-CrossesToPredict %$% union(sireID,damID)
   doseMat<-doseMat[parents,colnames(AddEffectList[[1]])]
 
-  require(furrr); options(mc.cores=ncores); plan(multicore)
-  means<-means %>%
-    mutate(predictedMeans=future_map(Trait,function(Trait,...){
-      parentGEBVs<-tcrossprod(doseMat,AddEffectList[[Trait]])
+  require(furrr); plan(multicore, workers = ncores)
+  options(future.globals.maxSize=+Inf); options(future.rng.onMisuse="ignore")
 
-      predmeans<-CrossesToPredict %>%
-        left_join(tibble(sireID=rownames(parentGEBVs),
-                         sireGEBV=as.numeric(parentGEBVs))) %>%
-        left_join(tibble(damID=rownames(parentGEBVs),
-                         damGEBV=as.numeric(parentGEBVs))) %>%
-        mutate(predOf="MeanBV",predMean=(sireGEBV+damGEBV)/2)
-      if(modelType=="AD"){
-        predmeans %<>%
-          bind_rows(predmeans %>%
-                      mutate(predOf="MeanTGV",
-                             predMean=map2_dbl(sireID,damID,function(sireID,damID,...){
-                               # Eqn 14.6 from Falconer+MacKay
-                               p1<-doseMat[sireID,]/2
-                               p2<-doseMat[damID,]/2
-                               q<-1-p1
-                               y<-p1-p2
-                               g<-AddEffectList[[Trait]]*(p1-q-y) + DomEffectList[[Trait]]*((2*p1*q)+y*(p1-q))
-                               meanG<-sum(g)
-                               return(meanG)})))
-      }
-      return(predmeans)})) %>%
+  if(predType=="BV"){
+    means<-means %>%
+      mutate(predictedMeans=future_map(Trait,function(Trait,...){
+        parentGEBVs<-tcrossprod(doseMat,AddEffectList[[Trait]])
+
+        predmeans<-CrossesToPredict %>%
+          left_join(tibble(sireID=rownames(parentGEBVs),
+                           sireGEBV=as.numeric(parentGEBVs))) %>%
+          left_join(tibble(damID=rownames(parentGEBVs),
+                           damGEBV=as.numeric(parentGEBVs))) %>%
+          mutate(predOf="MeanBV",
+                 predMean=(sireGEBV+damGEBV)/2)
+        return(predmeans) }))
+  }
+
+  if(predType=="TGV"){
+    means<-means %>%
+      mutate(predictedMeans=future_map(Trait,function(Trait,...){
+        predmeans<-CrossesToPredict %>%
+          mutate(predOf="MeanTGV",
+                 predMean=map2_dbl(sireID,damID,function(sireID,damID,...){
+                   # Eqn 14.6 from Falconer+MacKay
+                   p1<-doseMat[sireID,]/2
+                   p2<-doseMat[damID,]/2
+                   q<-1-p1
+                   y<-p1-p2
+                   g<-AddEffectList[[Trait]]*(p1-q-y) + DomEffectList[[Trait]]*((2*p1*q)+y*(p1-q))
+                   meanG<-sum(g)
+                   return(meanG)}))
+        return(predmeans) }))
+  }
+  means<-means %>%
     unnest(predictedMeans)
-  return(means) }
+  return(means)
+}
+
+#' getPropHom
+#'
+#' Compute the per-individual proportion homozygous.
+#' For example, to use as a predictor for a directional dominance model.
+#'
+#' @param M dosage matrix. Assumes SNPs in M coded 0, 1, 2 (requires rounding dosages to integers). M is Nind x Mrow, numeric matrix, with row/columanes to indicate SNP/ind ID
+#'
+#' @return vector of per-individual proportion homozygous
+#' @export
+#'
+#' @examples
+getPropHom<-function(M){
+  W<-M; W[which(W==2)]<-0;
+  # f = 1 − h/N,
+  # where N is the number of SNPs
+  f<-1-(rowSums(W)/ncol(W))
+  return(f)
+}
+
+#' centerDosage
+#'
+#' Centers dosage matrix, e.g. for use in whole-genome regressions like rrBLUP
+#'
+#' @param M dosage matrix. Assumes SNPs in M coded 0, 1, 2 (requires rounding dosages to integers). M is Nind x Mrow, numeric matrix, with row/columanes to indicate SNP/ind ID
+#'
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' centeredM<-centerDosage(M)
+centerDosage<-function(M){
+  freq <- colMeans(M,na.rm=T)/2
+  P <- matrix(rep(freq,nrow(M)),byrow=T,ncol=ncol(M))
+  Z <- M-2*P
+  return(Z)
+}
+
+#' dose2domDev
+#'
+#' Converts a dosage matrix into a matrix of centered dominance deviations.
+#' This sets up the "classical" (aka "Statistical") partition of additive dominance in terms of breeding values and dom. deviations.
+#' See function dose2domDevGenotypic() to set-up the "genotypic" (aka "biological") partition in terms of genotypic effects.
+#' See Vitezica et al. 2013.
+#' Also see Varona et al. 2018. https://doi.org/10.3389/fgene.2018.00078
+#'
+#' @param M dosage matrix. Assumes SNPs in M coded 0, 1, 2 (requires rounding dosages to integers). M is Nind x Mrow, numeric matrix, with row/columanes to indicate SNP/ind ID
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' domDev<-dose2domDev(M)
+dose2domDev<-function(M){
+  freq <- colMeans(M,na.rm=T)/2
+  P <- matrix(rep(freq,nrow(M)),byrow=T,ncol=ncol(M))
+  W<-M;
+  W[which(W==1)]<-2*P[which(W==1)];
+  W[which(W==2)]<-(4*P[which(W==2)]-2);
+  W <- W-2*(P^2)
+  return(W)
+}
+
+
+#' dose2domDevGenotypic
+#'
+#' Converts a dosage matrix into a matrix of centered dominance deviations.
+#' This sets up the "genotypic" (aka "biological") partition of additive dominance in terms of their genotypic effects instead of in terms of breeding values or dominance deviations.
+#' See function dose2domDev() to set-up the "statistical" (aka "classical") partition in terms of genotypic effects.
+#' See Vitezica et al. 2013.
+#' Also see Varona et al. 2018. https://doi.org/10.3389/fgene.2018.00078
+#'
+#' @param M dosage matrix. Assumes SNPs in M coded 0, 1, 2 (requires rounding dosages to integers). M is Nind x Mrow, numeric matrix, with row/columanes to indicate SNP/ind ID
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' domDev<-dose2domDevGenotypic(M)
+dose2domDevGenotypic<-function(M){
+  freq <- colMeans(M,na.rm=T)/2
+  P <- matrix(rep(freq,nrow(M)),byrow=T,ncol=ncol(M))
+  W<-M; W[which(W==2)]<-0;
+  W <- W-(2*P*(1-P))
+  return(W)
+}
+
+#' getAF
+#'
+#' get a vector of allele frequences from a dosage matrix
+#'
+#' @param M dosage matrix. Assumes SNPs in M coded 0, 1, 2 (requires rounding dosages to integers). M is Nind x Mrow, numeric matrix, with row/columanes to indicate SNP/ind ID
+#'
+#' @return vector of allele frequencies, names = SNP IDs if in cols of M
+#' @export
+#'
+#' @examples
+getAF<-function(M){ colMeans(M,na.rm=T)/2 }
+
+#' getMAF
+#'
+#' get a vector of _minor_ allele frequences from a dosage matrix
+#'
+#' @param M dosage matrix. Assumes SNPs in M coded 0, 1, 2 (requires rounding dosages to integers). M is Nind x Mrow, numeric matrix, with row/columanes to indicate SNP/ind ID
+#'
+#' @return vector of _minor_ allele frequencies, names = SNP IDs if in cols of M
+#' @export
+#'
+#' @examples
+getMAF<-function(M){
+  freq<-colMeans(M, na.rm=T)/2; maf<-freq;
+  maf[which(maf > 0.5)]<-1-maf[which(maf > 0.5)]
+  return(maf) }
+
+#' maf_filter
+#'
+#' filter a dosage matrix by minor allele frequence.
+#' get a vector of allele frequences from a dosage matrix
+#'
+#' @param M dosage matrix. Assumes SNPs in M coded 0, 1, 2 (requires rounding dosages to integers). M is Nind x Mrow, numeric matrix, with row/columanes to indicate SNP/ind ID
+#' @param thresh threshold value. Columns of M with maf<thresh will be removed
+#' @return dosage matrix potentially with columns removed
+#' @export
+#'
+#' @examples
+maf_filter<-function(M,thresh){
+  freq<-colMeans(M, na.rm=T)/2; maf<-freq;
+  maf[which(maf > 0.5)]<-1-maf[which(maf > 0.5)]
+  snps1<-M[,which(maf>thresh)];
+  return(snps1) }
+
+#' remove_invariant
+#'
+#' filter a dosage matrix, removing invariant markers. Removes e.g. cases where MAF=0.5 but all dosages == 1 (het).
+#'
+#' @param M dosage matrix. Assumes SNPs in M coded 0, 1, 2 (requires rounding dosages to integers). M is Nind x Mrow, numeric matrix, with row/columanes to indicate SNP/ind ID
+#' @param thresh threshold value. Columns of M with maf<thresh will be removed
+#' @return dosage matrix potentially with columns removed
+#' @export
+#'
+#' @examples
+remove_invariant<-function(M){
+  snps1<-M[ ,apply(M, 2, var) != 0]
+  return(snps1) }
+
+#' backsolveSNPeff
+#'
+#' From the GBLUP solutions and a centered SNP matrix backsolve SNP effects
+#'
+#' @param Z Centered marker matrix (dominance deviations must also be centered)
+#' @param g The solutions (blups, i.e. GEBVs) from the GBLUP model
+#'
+#' @return matrix of SNP effects matching RR-BLUP / SNP-BLUP
+#' @export
+#'
+#' @examples
+#' A<-kinship(M,type="add")
+#' trainingDF %<>% dplyr::mutate(ga=factor(as.character(id),
+#'                                         levels=rownames(A)),
+#'                               gd=ga)
+#' gblup<-mmer(pheno~1,
+#'             random=~vs(ga,Gu = A),
+#'             weights=WT,
+#'             data=trainingDF,verbose = T)
+#' ga<-as.matrix(gblup$U$`u:ga`$pheno,ncol=1)
+#' Za<-centerDosage(M)
+#' snpeff<-backsolveSNPeff(Za,ga)
+backsolveSNPeff<-function(Z,g){
+  ZZt<-tcrossprod(Z)
+  diag(ZZt)<-diag(ZZt)+1e-8
+  bslvEffs<-crossprod(Z,solve(ZZt))%*%g
+  return(bslvEffs)
+}
 
